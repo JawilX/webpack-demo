3ed55a48fb51223d3279a96fbb0e1c58c2f6d311
jiahui.xu <jiahui.xu@ecreditpal.com>
2020-06-19 14:33:55 +0800

    output-management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
 
   // lodash，现在通过一个 script 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+  btn.innerHTML = '点击这里，然后查看 console！'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
 
   return element
 }
