6b4c4f0847fbe4c117b9fc82a6204c84ab8932a9
jiahui.xu <jiahui.xu@ecreditpal.com>
2020-07-02 10:16:19 +0800

    tree-shaking

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import _ from 'lodash'
+import { cube } from "./math.js";
 
 function component() {
-  const element = document.createElement('div')
+  const element = document.createElement('pre')
 
   // lodash，现在通过一个 script 引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n')
 
   return element
 }
